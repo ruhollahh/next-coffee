fcc4b6102a1b6dfb81da942ef5cdfb59e820a1d7
ruhollahh <ruhollahh01@gmail.com>
2022-06-17 12:18:46 +0430

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#5c7f67",
+				secondary: {
+					DEFAULT: "#ecf4e7",
+					content: "#24331a",
+				},
+				accent: {
+					DEFAULT: "#fae5e5",
+					content: "#322020",
+				},
+				neutral: {
+					DEFAULT: "#5d5656",
+					content: "#e9e7e7",
+				},
+				"base-100": {
+					DEFAULT: "#e9e7e7",
+					content: "#100f0f",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
